486e4bb8c334340c582d3ff509f7844ed9a5d603
Taras Kozak <kozaktarasofficial@gmail.com>
2021-06-01 14:12:19 +0300

    Updates

--- precache-manifest.3f4a21946402cfad3b4fac24174dd25b.js
+++ precache-manifest.3f4a21946402cfad3b4fac24174dd25b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b98f5bc12f41b60932b09f183f63ddc0",
+    "revision": "ad3aa56648d6b11d6d876af013b4029e",
     "url": "./index.html"
   },
   {
-    "revision": "b3b6b4a1bed476bc3072",
+    "revision": "4510f9ef155766ebf4e4",
     "url": "./static/css/main.819e4dd6.chunk.css"
   },
   {
-    "revision": "965d47e073d551d08910",
-    "url": "./static/js/2.98e7adc1.chunk.js"
+    "revision": "56520c5f223d9c08cda6",
+    "url": "./static/js/2.3d236229.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "./static/js/2.98e7adc1.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.3d236229.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b3b6b4a1bed476bc3072",
-    "url": "./static/js/main.ec3f3a15.chunk.js"
+    "revision": "4510f9ef155766ebf4e4",
+    "url": "./static/js/main.4fff9df3.chunk.js"
   },
   {
     "revision": "cc2a5e26d5c4844758e0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bf2d532526c5f25df62aece305e3d226.js"
+  "./precache-manifest.3f4a21946402cfad3b4fac24174dd25b.js"
 );
 
 self.addEventListener('message', (event) => {
